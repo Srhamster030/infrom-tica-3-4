452fa20d8a97c66610ae8bc5617d7b95136f6aa0
Hamster <hamnoemireyeshrdz@gmail.com>
2025-05-02 19:59:05 +0000

    :3

---
+++ Repository 3/Leap Years.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    printf("Enter a year:");
+    int year; 
+    scanf("%d",&year);
+
+    if(year % 4 ==0 && year % 100 !=0){
+        printf("%d is a leap year.\n", year);
+    } elese {
+        printf("%d in NOT a leap year.\n",year);
+    }
+    }
+    if(year % 4 ==0){
+        printf("is a")
+    } else if (year %100)
+}
