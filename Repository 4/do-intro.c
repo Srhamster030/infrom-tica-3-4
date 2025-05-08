c9b9bada0c92c479f43b0023c785b6e4f8255685
Hamster <hamnoemireyeshrdz@gmail.com>
2025-05-08 16:10:50 +0000

    :3

---
+++ Repository 4/do-intro.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int number;
+
+    printf("Enter a positive number: ");
+    scanf("%d",&number);
+
+    int num = 1;
+    int answer = 0;
+
+    if(number > 0){
+        while(number/num >= 1){
+            num = num*10;
+            ++answer;
+        }
+        printf("%d\n", answer);
+    }else {
+        printf("NOT A POSITIVE NUMBER >:(\n)");
+    }
+    return 0;
+}
