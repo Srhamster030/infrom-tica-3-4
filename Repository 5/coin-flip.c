786c9e4331e81eab082f5bb8492ecef39b911be6
Hamster <hamnoemireyeshrdz@gmail.com>
2025-05-14 14:51:31 +0000

    :3

---
+++ Repository 5/coin-flip.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    srand(time(NULL));
+    scanf("%\,& coinFlip");
+    int coinFlip = (rand() % 2) + 1;
+     if( coinflip == 1){
+         printf("is 1 == tails")
+     }
+
+    return 0;
+}
--- Repository 5/random.c
+++ Repository 5/random.c
@@ -3,5 +3,7 @@
 #include <time.h>
 int main (){
     srand(time(NULL));
-    INT 
+    int diceRoll = (rand() % 6) + 1; 
+    printf("You rolled a: %d\n", diceRoll);
+    return 0;
 }
