687fad51a3e0e259a3065cc2352777825771053c
Hamster <hamnoemireyeshrdz@gmail.com>
2025-05-14 16:29:24 +0000

    :3

--- Repository 5/coin-flip.c
+++ Repository 5/coin-flip.c
@@ -1,11 +1,22 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
+
 int main (){
-    srand(time(NULL));
-    scanf("%\,& coinFlip");
-    int coinFlip = (rand() % 2) + 1;
-     if( coinflip == 1){
-         printf("is 1 == tails")
+     srand(time(NULL)); 
+
+     printf("tails = 1");
+    ///
+     printf("head = 2");
+
+int coinFlip = (rand() % 2) + 1;
+     if(coinFlip == 1){
+         printf("tails");
+        } 
+     else (coinFlip == 2);{
+        printf("heads");
      }
 
     return 0;
 }
+;C 
