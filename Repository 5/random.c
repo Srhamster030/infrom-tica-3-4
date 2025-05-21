f39a94f083958e31606135231b49807a7b637aa4
Hamster <hamnoemireyeshrdz@gmail.com>
2025-05-21 15:29:18 +0000

    :3

--- Repository 5/random.c
+++ Repository 5/random.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int main (){
+
+int main() {
     srand(time(NULL));
-    int diceRoll = (rand() % 6) + 1; 
-    printf("You rolled a: %d\n", diceRoll);
+    int rolls;
+    printf("Enter the number of dice rolls: ");
+    scanf("%d", &rolls);
+    for(;rolls > 0; -- rolls){
+    int diceRoll = (rand() %6) + 1;
+    printf("You rolled a: %d\n", diceRoll);}
     return 0;
 }
